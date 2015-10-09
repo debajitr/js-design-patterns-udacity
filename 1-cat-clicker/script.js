f9c26b72806a31c5a5d3f39baa669c574637eea7
debajit.roy <debajit.roy@outlook.com>
2015-10-09 12:14:48 +0530

    Refactoring * Css changes and script cleaned

--- 1-cat-clicker/script.js
+++ 1-cat-clicker/script.js
@@ -6,9 +6,16 @@
   var catClick = 0;
   var catImage = document.getElementById('cat-image');
   var clickCounter = document.getElementsByClassName('click-counter');
+  var clickContainer = document.getElementById("click-container");
+  var clickEvent = 'click';
+  var hideClass = 'hide';
 
-  catImage.addEventListener('click', function () {
+  catImage.addEventListener(clickEvent, function () {
     catClick++;
     clickCounter[0].innerHTML = catClick;
+
+    if (clickContainer.classList.contains(hideClass)) {
+      clickContainer.classList.remove(hideClass);
+    }
   })
 })();
